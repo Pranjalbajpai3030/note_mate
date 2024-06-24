58341c23ee09ad16270d56da5259e67ae6d3894f
Pranjalbajpai3030 <127866867+Pranjalbajpai3030@users.noreply.github.com>
2024-06-24 22:55:02 +0530

    creating schema

--- Backend/index.js
+++ Backend/index.js
@@ -2,13 +2,14 @@ const connectToMongo = require('./db');
 const express = require('express');
 
 connectToMongo();
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3001
+app.use(express.json())
 
-app.get('/', (req, res) => {
-  res.send('Hello   Pranjal!');
-});
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
+
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+  console.log(`Example app listening at http://localhost:${port}`)
+})
