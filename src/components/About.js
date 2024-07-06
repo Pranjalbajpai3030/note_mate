7c6f9957a583c53452b8e6ae47a39892fbf8675f
Pranjalbajpai3030 <127866867+Pranjalbajpai3030@users.noreply.github.com>
2024-07-06 21:35:40 +0530

    added logo and some css

--- src/components/About.js
+++ src/components/About.js
@@ -2,12 +2,14 @@ import React  from 'react';
 
 
 
+
 function About() {
  
 
     return (
         <div>
-            <h1>About Us</h1>
+            
+       
            
         </div>
     );
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,12 @@
+import React from 'react';
 import Notes from './Notes';
+import logo from './logo.png'; // Adjust the path if logo.png is in a different directory
+
 export const Home = () => {
     return (
-        <div>
-        
-            <Notes/>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', marginTop: '50px', marginBottom: '50px' }}>
+            <img src={logo} alt="Logo" style={{ width: '70%', maxWidth: '500px' }} />
+            <Notes />
         </div>
     );
 }
